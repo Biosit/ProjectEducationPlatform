c2ddc1ff81ed7e1a117cb0acf05fc0c963cbaa72
Jimmy-Parrales <jimmi1939@gmail.com>
2022-10-08 02:16:25 +0000

    querys

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,55 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    usuario = db.Column(db.String(120), unique=True, nullable=False)
+    correo = db.Column(db.String(80), unique=True, nullable=False)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    perfil = db.Column(db.String(120), unique=False, nullable=False)
+    clave = db.Column(db.String(120), unique=True, nullable=False)
+   # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.correo}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "usuario": self.usuario,
+            "correo": self.correo,
+            "nombre": self.nombre,
+            "perfil": self.perfil,
+            "clave": self.clave,
+            # do not serialize the password, its a security breach
+        }
+
+class Calificaciones(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    inscripcion=db.Column(db.Boolean, unique=False, nullable=False)
+    actividad=db.Column(db.Boolean(), unique=False, nullable=False)
+    calificacion=db.Column(db.Integer,nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.inscripcion}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "inscripcion": self.inscripcion,
+            "actividad": self.actividad,
+            "calificacion": self.calificacion,
+            # do not serialize the password, its a security breach
+        }
+
+class Inscribir(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    perfil = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "perfil":self.perfil
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Calificaciones, Inscribir
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,22 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/Users', methods=['GET'])
+def Users():
+    usuarios=User.query.all()
+    usuarios=list(map(lambda Usuarios: Usuarios.serialize(), usuarios))    
+    return jsonify(usuarios), 200
+
+@api.route('/notas', methods=['GET'])
+def Notas():
+    notas=Calificaciones.query.all()
+    notas=list(map(lambda puntaje: puntaje.serialize(), notas))    
+    return jsonify(notas), 200
+
+
+@api.route('/inscripciones', methods=['GET'])
+def inscripciones():
+    inscripcion=Inscribir.query.all()
+    inscripcion=list(map(lambda inscrito: inscrito.serialize(), inscripcion))    
+    return jsonify(inscripcion), 200
